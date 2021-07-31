256f099f5adc58e31350ea61f5846d60aa17345b
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-01 02:44:40 +0300

    Config cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", 
-domain: "rails-react-authentication.herokuapp.com"
+if Rails.env =="production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app",
+  domain: "rails-react-authentication.herokuapp.com"
+else 
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
